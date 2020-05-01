90b048da1799d80b891238dd4fb905b6d403de28
Madara Priede <priede.madara@gmail.com>
2020-05-01 14:05:42 +0300

    Day 5  - Classwork #3

--- MD/Day5_Arrays/Day5_Arrays/Program.cs
+++ MD/Day5_Arrays/Day5_Arrays/Program.cs
@@ -7,7 +7,44 @@ namespace Day5_Arrays
         static void Main(string[] args)
         {
             //ArrayInput();
-            Average();
+            //Average();
+            FindThePosition();
+        }
+
+
+        static void FindThePosition()
+        {
+            int[] randoms = new int[10];
+            for(int i=0; i<randoms.Length; i++)
+            {
+                Random random = new Random();
+                randoms[i] = random.Next(1, 11);
+            }
+            Console.WriteLine("Which number are you looking for?");
+            int position = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Positions in array:");
+
+            for (int i =0; i<randoms.Length; i++)
+            {
+                if (randoms[i] == position)
+                    Console.Write(i);
+            }
+            
+        }
+        static void Average()
+        {
+            int sum = 0;
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+
+            int average = sum / array.Length;
+            Console.WriteLine("The sum is: " + sum);
+            Console.WriteLine("The average is: " + average);
+
         }
 
         static void ArrayInput()
@@ -30,20 +67,6 @@ namespace Day5_Arrays
 
         }
 
-        static void Average()
-        {
-            int sum = 0;
-            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-            for(int i=0; i< array.Length; i++)
-            {
-                sum = sum + array[i];
-            }
-            
-            int average = sum / array.Length;
-            Console.WriteLine("The sum is: " + sum);
-            Console.WriteLine("The average is: " + average);
-            
-        }
+        
     }
 }
