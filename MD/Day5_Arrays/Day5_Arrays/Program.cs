cd00797428af36dbfd81cfe5329f2d0855942906
Madara Priede <priede.madara@gmail.com>
2020-05-01 13:02:13 +0300

    Day 5 - Classwork

---
+++ MD/Day5_Arrays/Day5_Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Day5_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayInput();
+        }
+
+        static void ArrayInput()
+        {
+            int[] a = new int[5];
+            int sum = 0;
+
+            for (int i = 0; i<=4; i++)
+            {
+                Console.WriteLine("Please input your number:");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+                
+            }
+
+            for(int i = 0; i<=4; i++)
+            {
+                sum = sum + a[i];
+            }
+            Console.Write("The sum of your numbers is: " + sum);
+
+        }
+    }
+}
