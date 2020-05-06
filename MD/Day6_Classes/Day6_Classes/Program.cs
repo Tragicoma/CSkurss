f26092c882770ef335a4f4c5ecfd57b5446e1083
Madara Priede <priede.madara@gmail.com>
2020-05-06 17:31:46 +0300

    Day6 - 1#CW

---
+++ MD/Day6_Classes/Day6_Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day6_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! \n Please, insert the amount of rows:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Task.stars(rows);
+
+        }
+    }
+}
