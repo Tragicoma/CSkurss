1c16525a15a9897c8d33c880919b6df331ffb06f
Madara Priede <priede.madara@gmail.com>
2020-05-27 17:51:35 +0300

    Day 14 - CW

---
+++ MD/Day14_GUILists/Day14_GUILists/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Day14_GUILists
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list = new System.Windows.Forms.ListView();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // list
+            // 
+            this.list.HideSelection = false;
+            this.list.Location = new System.Drawing.Point(39, 29);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(192, 397);
+            this.list.TabIndex = 0;
+            this.list.UseCompatibleStateImageBehavior = false;
+            this.list.View = System.Windows.Forms.View.List;
+            this.list.MouseClick += new System.Windows.Forms.MouseEventHandler(this.list_MouseClick);
+            this.list.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.list_MouseDoubleClick);
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(326, 69);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(104, 25);
+            this.btnClick.TabIndex = 1;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(246, 29);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(184, 20);
+            this.textBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Double-click to remove item";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.list);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView list;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MD/Day14_GUILists/Day14_GUILists/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day14_GUILists
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool selected = false;
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            bool print = true;
+
+            if(textBox.Text.Length<1)
+            {
+                print = false;
+            }
+
+            if(print&&!selected)
+            {
+                list.Items.Add(textBox.Text);
+            }
+            else if(selected)
+            {
+                list.SelectedItems[0].Text = textBox.Text;
+            }
+
+        }
+
+        private void list_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox.Text = list.SelectedItems[0].Text;
+            selected = true;
+        }
+
+        private void list_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            list.Items.Remove(list.SelectedItems[0]);
+        }
+    }
+}
