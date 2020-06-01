bfe5af55035a9a30ccbd6c524540b29a5ecc6c10
Madara Priede <priede.madara@gmail.com>
2020-06-01 03:09:13 +0300

    Day15_WF_Homework

---
+++ MD/Day15_IOWinForms/Day15_IOWinForms/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Day15_IOWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.FileBox = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnReverse = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(38, 95);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(602, 329);
+            this.TextBox.TabIndex = 0;
+            this.TextBox.Text = "";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(661, 31);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(127, 30);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // FileBox
+            // 
+            this.FileBox.Location = new System.Drawing.Point(38, 37);
+            this.FileBox.Name = "FileBox";
+            this.FileBox.Size = new System.Drawing.Size(602, 20);
+            this.FileBox.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(661, 131);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(127, 30);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnReverse
+            // 
+            this.btnReverse.Location = new System.Drawing.Point(661, 95);
+            this.btnReverse.Name = "btnReverse";
+            this.btnReverse.Size = new System.Drawing.Size(127, 30);
+            this.btnReverse.TabIndex = 5;
+            this.btnReverse.Text = "Reverse Text";
+            this.btnReverse.UseVisualStyleBackColor = true;
+            this.btnReverse.Click += new System.EventHandler(this.btnReverse_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Location = new System.Drawing.Point(661, 167);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(127, 30);
+            this.btnSaveAs.TabIndex = 6;
+            this.btnSaveAs.Text = "Save As";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnReverse);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.FileBox);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.TextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextBox;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox FileBox;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnReverse;
+        private System.Windows.Forms.Button btnSaveAs;
+    }
+}
+
---
+++ MD/Day15_IOWinForms/Day15_IOWinForms/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day15_IOWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string fp;
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog folderDlg = new OpenFileDialog();
+            folderDlg.InitialDirectory = @"D:\Jauna mape\C#_Maaciibas\C--kurss\MD\Day15_IO";
+           
+            if (folderDlg.ShowDialog()==DialogResult.OK)
+            {
+                
+                FileBox.Text = folderDlg.FileName;
+                fp = folderDlg.FileName;
+                TextBox.Clear();
+                List<string> list = File.ReadAllLines(fp).ToList();
+
+                foreach (string s in list)
+                {
+                    TextBox.Text += s + "\n";
+                }
+            }
+
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(fp, TextBox.Text);
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+            list = TextBox.Text.Split('\n').ToList();
+            //  \n's at the end are erased  
+            if (list[list.Count - 1].Length <  1 )
+            { list.Remove(list[list.Count - 1]); }
+
+            list.Reverse();
+            TextBox.Clear();
+
+            foreach (string s in list)
+            {
+                TextBox.Text += s + "\n";
+            }
+            
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog folderDlg = new SaveFileDialog();
+            folderDlg.InitialDirectory = @"D:\Jauna mape\C#_Maaciibas\C--kurss\MD\Day15_IO";
+            folderDlg.ShowDialog();
+            if(folderDlg.FileName!="")
+            {
+                File.WriteAllText(folderDlg.FileName, TextBox.Text);
+            }
+
+        }
+    }
+}
